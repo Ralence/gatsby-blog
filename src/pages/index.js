7574b40b8b51b24816c4d4ab75e8f114b4f50b23
Ralence <ranko.ostojic@gmail.com>
2020-07-15 22:54:34 +0200

    feat: add hero image box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import PostPreview from '../components/PostPreview';
 import usePosts from '../hooks/use-posts';
+import Hero from '../components/Hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Frontend Masters!</p>
-      <Link to="/about">About</Link>
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <Fragment>
+      <Hero></Hero>
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </Fragment>
   );
 };
